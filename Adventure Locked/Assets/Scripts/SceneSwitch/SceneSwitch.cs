f7c9db3cfc6afd580a56a81542c3d88baad662bf
Wes <wmont012@ucr.edu>
2021-09-25 01:31:19 -0700

    Random Encounters Work!
    Overworld Size finished. Encounters have a 5% chance to appear per adjacent tile.

---
+++ Adventure Locked/Assets/Scripts/SceneSwitch/SceneSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float chance;
+    void OnTriggerEnter2D(Collider2D other){
+    	
+    	chance=Random.Range(1,100);
+    	Debug.Log("Collided!" + chance);
+    	if(chance<5){
+    		SceneManager.LoadScene(1);
+    	}
+    	
+    }
+}
